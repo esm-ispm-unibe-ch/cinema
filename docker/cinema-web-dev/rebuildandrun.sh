d88d5ed15e6027280e237c463cf1f87f4e10b962
Thodoris Papakonstantinou <tpapak@gmail.com>
2019-05-10 14:54:07 +0200

    changed url in welcome page

---
+++ docker/cinema-web-dev/rebuildandrun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /root/cinema/webapp
+#pull from github https://github.com/esm-ispm-unibe-ch/cinema.git
+git pull
+#first build purescripts with pulp
+cd /root/cinema/webapp/app/scripts/purescripts
+bower install --allow-root
+npx pulp build
+#then build scripts with gulp
+cd /root/cinema/webapp
+npm install &&\
+ npm rebuild node-sass
+bower install --allow-root
+npm i npx
+gulp build
+#reload nginx
+mv dist cinema 
+rm -rf /usr/share/nginx/cinema 
+mv cinema /usr/share/nginx/
+nginx
+nginx -s reload
